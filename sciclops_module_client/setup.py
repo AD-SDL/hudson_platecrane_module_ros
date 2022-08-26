cd5301593451e1d87d2f6d48d102903125a2334f
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-08-26 12:11:24 -0500

    fix launch

--- sciclops_module_client/setup.py
+++ sciclops_module_client/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+import glob
 
 package_name = 'sciclops_module_client'
 
@@ -11,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
