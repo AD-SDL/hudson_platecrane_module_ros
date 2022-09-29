87c0adc36dabc03a673db15a5935b8a8bd3157a0
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-09-28 20:47:21 -0500

    test new setup

--- sciclops_driver/setup.py
+++ sciclops_driver/setup.py
@@ -1,22 +1,11 @@
-import os
 from setuptools import setup, find_packages
 
-
-install_requires = []
-with open('requirements.txt') as reqs:
-    for line in reqs.readlines():
-        req = line.strip()
-        if not req or req.startswith('#'):
-            continue
-        install_requires.append(req)
-
-
-##this is weird. 
 package_name = 'sciclops_driver'
+install_requires = ['pyusb']
 
 setup(
     name='sciclops_driver',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -26,20 +15,10 @@ setup(
     install_requires=install_requires,
     zip_safe=True,
     python_requires=">=3.8",
-    maintainer='Rafael Vescovi and Doga Ozgulbas',
-    maintainer_email='dozgulbas@anl.gov',
-    description='Driver for the Azenta Sealer and Peeler',
-    url='https://github.com/AD-SDL/azenta_driver.git', 
+    maintainer='Rafael Vescovi and Abe Stoka',
+    maintainer_email='ravescovi@anl.gov',
+    description='Driver for the Sciclops',
+    url='https://github.com/AD-SDL/sciclops_module.git', 
     license='MIT License',
     entry_points={},
-    classifiers=[
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
 )
