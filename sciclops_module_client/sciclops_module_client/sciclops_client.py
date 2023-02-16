e9e8dec62fa42ef980dfe1d1cb13df9fea93cc1f
Dozgulbas <dozgulbas@anl.gov>
2023-02-16 15:01:57 -0600

    Changing the publisher timer

--- sciclops_module_client/sciclops_module_client/sciclops_client.py
+++ sciclops_module_client/sciclops_module_client/sciclops_client.py
@@ -63,7 +63,7 @@ class ScilopsClient(Node):
         state_refresher_cb_group = ReentrantCallbackGroup()
 
 
-        timer_period = 0.5 # seconds
+        timer_period = 0.7 # seconds
         self.statePub = self.create_publisher(String, node_name + '/state', 10)
         self.stateTimer = self.create_timer(timer_period, self.stateCallback, callback_group = state_cb_group)
         
