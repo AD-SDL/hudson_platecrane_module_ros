2878aaa83a0a320ee43d85a2f8ae01623c7bb8b4
Dozgulbas <dozgulbas@anl.gov>
2023-05-11 14:38:50 -0500

    Working on setting hights

--- platecrane_driver/platecrane_driver/platecrane_driver.py
+++ platecrane_driver/platecrane_driver/platecrane_driver.py
@@ -868,13 +868,13 @@ if __name__ == "__main__":
     # s.get_location_joint_values("HidexNest2")
     # s.set_location("HidexNest2", R=210015,Z=-30145,P=490,Y=2331) 
 
-    s.transfer("Stack5", solo4, source_type = "stack", target_type = "module", plate_type = "96_well")
-    # s.remove_lid(source = solo4, target="LidNest2", plate_type="96_well")
+    # s.transfer("Stack5", solo4, source_type = "stack", target_type = "module", plate_type = "96_well")
+    s.remove_lid(source = solo4, target="LidNest2", plate_type="96_well")
     # s.transfer("Stack4", solo3, source_type = "stack", target_type = "stack", plate_type = "tip_box_lid_off")
     # s.remove_lid(source = solo6, target="LidNest3", plate_type="tip_box_lid_on")
     # s.replace_lid(source = "LidNest3", target = solo6, plate_type = "tip_box_lid_on")
-    # s.replace_lid(source = "LidNest2", target = solo4, plate_type = "96_well")
-    s.transfer(solo4, "Stack5", source_type = "module", target_type = "stack", plate_type = "96_well")
+    s.replace_lid(source = "LidNest2", target = solo4, plate_type = "96_well")
+    # s.transfer(solo4, "Stack5", source_type = "module", target_type = "stack", plate_type = "96_well")
     # s.transfer(solo6, "Stack2", source_type = "module", target_type = "stack", plate_type = "tip_box_lid_on")
 
 
